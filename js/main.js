cf68dd8e7fa1c5e4dcabf08704b55cad6eb6b4d0
Kim Gyeong Un <kkw3071@gmail.com>
2020-12-07 16:57:38 +0900

    부트스트랩 제거(cdn으로 변경), 추가 폼 생성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function($) {
     'use strict';
 
-    function remain(dday){
+    function remain(id, dday){
         const now = new Date(); 
         
         const days = (dday - now) / 1000 / 60 / 60 / 24; 
@@ -12,14 +12,17 @@
         const minutesRound = Math.floor(minutes); 
         const seconds = (dday - now) / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound); 
         
-        document.getElementById('text').innerHTML = daysRound.toString().padStart(3, 0) + ' : ' 
+        $(id).text(daysRound.toString().padStart(3, 0) + ' : ' 
             + hoursRound.toString().padStart(2, 0) + ' : ' 
             + minutesRound.toString().padStart(2, 0) + ' : ' 
-            + Math.floor(seconds).toString().padStart(2, 0);
-        document.getElementsByTagName('title').item(0).text = 'D-' + daysRound;
+            + Math.floor(seconds).toString().padStart(2, 0));
     }
     
     const dday = new Date(2021, 10, 18, 8, 40, 0);
-    remain(dday);
-    setInterval(() => remain(dday), 1000);
+    remain('#default', dday);
+    setInterval(() => remain('#default', dday), 1000);
+
+    $(document).on('click', '#add', () => {
+        alert('추가 예정');
+    });
 })(jQuery);
