8984e2addf2c6c9a944ed4ba27d5396ba6e3021c
Kim Gyeong Un <kkw3071@gmail.com>
2020-12-07 16:10:29 +0900

    const 추가

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@
     'use strict';
 
     function remain(dday){
-        now = new Date(); 
+        const now = new Date(); 
         
-        days = (dday - now) / 1000 / 60 / 60 / 24; 
-        daysRound = Math.floor(days); 
-        hours = (dday - now) / 1000 / 60 / 60 - (24 * daysRound); 
-        hoursRound = Math.floor(hours); 
-        minutes = (dday - now) / 1000 /60 - (24 * 60 * daysRound) - (60 * hoursRound); 
-        minutesRound = Math.floor(minutes); 
-        seconds = (dday - now) / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound); 
+        const days = (dday - now) / 1000 / 60 / 60 / 24; 
+        const daysRound = Math.floor(days); 
+        const hours = (dday - now) / 1000 / 60 / 60 - (24 * daysRound); 
+        const hoursRound = Math.floor(hours); 
+        const minutes = (dday - now) / 1000 /60 - (24 * 60 * daysRound) - (60 * hoursRound); 
+        const minutesRound = Math.floor(minutes); 
+        const seconds = (dday - now) / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound); 
+        
         document.getElementById('text').innerHTML = daysRound.toString().padStart(3, 0) + ' : ' 
             + hoursRound.toString().padStart(2, 0) + ' : ' 
             + minutesRound.toString().padStart(2, 0) + ' : ' 
